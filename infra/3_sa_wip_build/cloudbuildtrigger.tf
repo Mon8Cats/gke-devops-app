204804dd58f6745808847cd5497645c695fe9c5d
= <=>
2024-10-19 17:29:08 -0500

    add providers

--- infra/3_sa_wip_build/cloudbuildtrigger.tf
+++ infra/3_sa_wip_build/cloudbuildtrigger.tf
@@ -2,7 +2,7 @@
 
 
 # Step 7: Create a Cloud Build Trigger
-resource "google_cloudbuild_trigger" "githubtrigger" {
+resource "google_cloudbuild_trigger" "github_trigger" {
   location = var.region
   name        = "github-trigger-main-branch"
   description = "Trigger for building and deploying when code is pushed to the main branch"
